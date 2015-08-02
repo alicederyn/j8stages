c11f4384335288a1f95c919c153ca523907850d1
Chris Purcell <Chris.Purcell.39@gmail.com>
2015-08-02 23:47:27 +0100

    Add a test to ensure the list of suppressed exceptions in a Throwable is not accidentally aliased during the shallow copy.

--- src/test/java/org/inferred/cjp39/j8stages/ExceptionClonerTest.java
+++ src/test/java/org/inferred/cjp39/j8stages/ExceptionClonerTest.java
@@ -30,4 +30,18 @@ public class ExceptionClonerTest {
         clone.addSuppressed(new RuntimeException("dormouse"));
         assertEquals(0, x.getSuppressed().length);
     }
+
+    @Test
+    public void suppressed_array_not_aliased() {
+        MyUnfriendlyException x =
+                new MyUnfriendlyException("Clone this, baby!", new RuntimeException("cause"));
+        x.addSuppressed(new RuntimeException("Help, help, I'm being repressed!"));
+        MyUnfriendlyException clone = ExceptionCloner.clone(x);
+        clone.addSuppressed(new RuntimeException("dormouse"));
+        MyUnfriendlyException clone2 = ExceptionCloner.clone(clone);
+        clone2.addSuppressed(new RuntimeException("in a teapot"));
+        assertEquals(3, clone2.getSuppressed().length);
+        assertEquals(2, clone.getSuppressed().length);
+        assertEquals(1, x.getSuppressed().length);
+    }
 }
